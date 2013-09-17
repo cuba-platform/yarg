b3cfdb02a1bc55eefe02c4a9a8ba1a15f2532fc7
degtyarjov <degtyarjov@haulmont.com>
2013-09-17 16:08:19 +0400

    merge parameter handling logic with platform jpql logic

--- core/src/com/haulmont/yarg/loaders/impl/AbstractDbDataLoader.java
+++ core/src/com/haulmont/yarg/loaders/impl/AbstractDbDataLoader.java
@@ -3,10 +3,9 @@ package com.haulmont.yarg.loaders.impl;
 import com.haulmont.yarg.exception.DataLoadingException;
 import com.haulmont.yarg.structure.BandData;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author degtyarjov
@@ -46,11 +45,89 @@ public abstract class AbstractDbDataLoader extends AbstractDataLoader {
         return outputData;
     }
 
+    protected QueryPack prepareQuery(String query, BandData parentBand, Map<String, Object> reportParams) {
+        Map<String, Object> currentParams = new HashMap<String, Object>();
+        if (reportParams != null) {
+            currentParams.putAll(reportParams);
+        }
+
+        //adds parameters from parent bands hierarchy
+        while (parentBand != null) {
+            addParentBandDataToParameters(parentBand, currentParams);
+            parentBand = parentBand.getParentBand();
+        }
+
+        List<QueryParameter> queryParameters = new ArrayList<QueryParameter>();
+        List<Object> values = new ArrayList<Object>();
+
+        int paramCount = 1;
+        for (Map.Entry<String, Object> entry : currentParams.entrySet()) {
+            // Remembers ${alias} positions
+            String paramName = entry.getKey();
+            Object paramValue = entry.getValue();
+
+            String alias = "${" + paramName + "}";
+            String paramNameRegexp = "\\$\\{" + paramName + "\\}";
+            String deleteRegexp = "(?i)\\s*(and|or)?\\s+[\\w|\\d|\\.|\\_]+\\s+(=|>=|<=|like|>|<)\\s*" + paramNameRegexp;
+
+            //if value == null - removing condition from query
+            if (paramValue == null) {
+                // remove unused null parameter
+                query = query.replaceAll(deleteRegexp, "");
+            } else if (query.contains(alias)) {
+                Pattern pattern = Pattern.compile(paramNameRegexp);
+                Matcher matcher = pattern.matcher(query);
+
+                int subPosition = 0;
+                while (matcher.find(subPosition)) {
+                    queryParameters.add(new QueryParameter(paramNameRegexp, paramCount++, convertParameter(paramValue)));
+                    subPosition = matcher.end();
+                }
+            }
+        }
+
+        // Sort params by position
+        Collections.sort(queryParameters, new Comparator<QueryParameter>() {
+            @Override
+            public int compare(QueryParameter o1, QueryParameter o2) {
+                return o1.getPosition().compareTo(o2.getPosition());
+            }
+        });
+
+        for (QueryParameter parameter : queryParameters) {
+            query = insertParameterToQuery(query, parameter);
+        }
+
+        query = query.trim();
+        if (query.endsWith("where")) {
+            query = query.replace("where", "");
+        }
+
+        return new QueryPack(query, queryParameters.toArray(new QueryParameter[queryParameters.size()]));
+    }
+
+    protected String insertParameterToQuery(String query, QueryParameter parameter) {
+        if (parameter.isSingleValue()) {
+            // Replace single parameter with ?
+            query = query.replaceAll(parameter.getParamRegexp(), "?");
+        } else {
+            // Replace multiple parameter with ?,..(N)..,?
+            List<?> multipleValues = parameter.getMultipleValues();
+            StringBuilder builder = new StringBuilder();
+            for (Object value : multipleValues) {
+                builder.append("?,");
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            query = query.replaceAll(parameter.getParamRegexp(), builder.toString());
+        }
+        return query;
+    }
+
     protected static class QueryPack {
         private String query;
-        private Object[] params;
+        private QueryParameter[] params;
 
-        public QueryPack(String query, Object[] params) {
+        public QueryPack(String query, QueryParameter[] params) {
             this.query = query;
             this.params = params;
         }
@@ -59,8 +136,50 @@ public abstract class AbstractDbDataLoader extends AbstractDataLoader {
             return query;
         }
 
-        public Object[] getParams() {
+        public QueryParameter[] getParams() {
             return params;
+        }
+    }
+
+    protected static class QueryParameter {
+        private Integer position;
+        private Object value;
+        private String paramRegexp;
+
+        public QueryParameter(String paramRegexp, Integer position, Object value) {
+            this.position = position;
+            this.value = value;
+            this.paramRegexp = paramRegexp;
+        }
+
+        public Integer getPosition() {
+            return position;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public String getParamRegexp() {
+            return paramRegexp;
+        }
+
+        public boolean isSingleValue() {
+            return !(value instanceof Collection || value instanceof Object[]);
+        }
+
+        public List<?> getMultipleValues() {
+            if (isSingleValue()) {
+                return Collections.singletonList(value);
+            } else {
+                if (value instanceof Collection) {
+                    return new ArrayList<Object>((Collection<?>) value);
+                } else if (value instanceof Object[]) {
+                    return Arrays.asList((Object[]) value);
+                }
+            }
+
+            return null;
         }
     }
 }
--- core/src/com/haulmont/yarg/loaders/impl/SqlDataLoader.java
+++ core/src/com/haulmont/yarg/loaders/impl/SqlDataLoader.java
@@ -1,8 +1,8 @@
 package com.haulmont.yarg.loaders.impl;
 
 import com.haulmont.yarg.exception.DataLoadingException;
-import com.haulmont.yarg.structure.ReportQuery;
 import com.haulmont.yarg.structure.BandData;
+import com.haulmont.yarg.structure.ReportQuery;
 import com.haulmont.yarg.util.db.QueryRunner;
 import com.haulmont.yarg.util.db.ResultSetHandler;
 import org.apache.commons.lang.StringUtils;
@@ -11,9 +11,10 @@ import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author degtyarjov
@@ -38,9 +39,19 @@ public class SqlDataLoader extends AbstractDbDataLoader {
 
         QueryRunner runner = new QueryRunner(dataSource);
         try {
-            QueryPack pack = prepareNativeQuery(query, parentBand, params);
+            QueryPack pack = prepareQuery(query, parentBand, params);
 
-            resList = runner.query(pack.getQuery(), pack.getParams(), new ResultSetHandler<List>() {
+            ArrayList<Object> resultingParams = new ArrayList<>();
+            QueryParameter[] queryParameters = pack.getParams();
+            for (QueryParameter queryParameter : queryParameters) {
+                if (queryParameter.isSingleValue()) {
+                    resultingParams.add(queryParameter.getValue());
+                } else {
+                    resultingParams.addAll(queryParameter.getMultipleValues());
+                }
+            }
+
+            resList = runner.query(pack.getQuery(), resultingParams.toArray(), new ResultSetHandler<List>() {
                 @Override
                 public List handle(ResultSet rs) throws SQLException {
                     List<Object[]> resList = new ArrayList<Object[]>();
@@ -68,121 +79,5 @@ public class SqlDataLoader extends AbstractDbDataLoader {
         }
 
         return fillOutputData(resList, outputParameters);
-    }
-
-    protected QueryPack prepareNativeQuery(String query, BandData parentBand, Map<String, Object> reportParams) throws SQLException {
-        Map<String, Object> currentParams = new HashMap<String, Object>();
-        if (reportParams != null) {
-            currentParams.putAll(reportParams);
-        }
-
-        //adds parameters from parent bands hierarchy
-        while (parentBand != null) {
-            addParentBandDataToParameters(parentBand, currentParams);
-            parentBand = parentBand.getParentBand();
-        }
-
-        List<QueryParameter> queryParameters = new ArrayList<QueryParameter>();
-        List<Object> values = new ArrayList<Object>();
-        for (Map.Entry<String, Object> entry : currentParams.entrySet()) {
-            // Remembers ${alias} positions
-            String paramName = entry.getKey();
-            Object paramValue = entry.getValue();
-
-            String alias = "${" + paramName + "}";
-            String paramNameRegexp = "\\$\\{" + paramName + "\\}";
-            String deleteRegexp = "(?i) *(and|or)? +[\\w|\\d|\\.|\\_]+ +(=|>=|<=|like) *" + paramNameRegexp;
-
-            //if value == null - removing condition from query
-            if (paramValue == null) {
-                // remove unused null parameter
-                query = query.replaceAll(deleteRegexp, "");
-            } else if (query.contains(alias)) {
-                Pattern pattern = Pattern.compile(paramNameRegexp);
-                Matcher matcher = pattern.matcher(query);
-
-                int subPosition = 0;
-                while (matcher.find(subPosition)) {
-                    queryParameters.add(new QueryParameter(paramNameRegexp, matcher.start(), convertParameter(paramValue)));
-                    subPosition = matcher.end();
-                }
-            }
-        }
-
-        // Sort params by position
-        Collections.sort(queryParameters, new Comparator<QueryParameter>() {
-            @Override
-            public int compare(QueryParameter o1, QueryParameter o2) {
-                return o1.getPosition().compareTo(o2.getPosition());
-            }
-        });
-
-        for (QueryParameter parameter : queryParameters) {
-            if (parameter.isSingleValue()) {
-                // Replace single parameter with ?
-                query = query.replaceAll(parameter.getParamRegexp(), "?");
-                Object value = parameter.getValue();
-                values.add(value);
-            } else {
-                // Replace multiple parameter with ?,..(N)..,?
-                List<?> multipleValues = parameter.getMultipleValues();
-                StringBuilder builder = new StringBuilder();
-                for (Object value : multipleValues) {
-                    builder.append("?,");
-                    values.add(value);
-                }
-                builder.deleteCharAt(builder.length() - 1);
-                query = query.replaceAll(parameter.getParamRegexp(), builder.toString());
-            }
-        }
-
-        query = query.trim();
-        if (query.endsWith("where")) {
-            query = query.replace("where", "");
-        }
-
-        return new QueryPack(query, values.toArray());
-    }
-
-    private static class QueryParameter {
-        private Integer position;
-        private Object value;
-        private String paramRegexp;
-
-        private QueryParameter(String paramRegexp, Integer position, Object value) {
-            this.position = position;
-            this.value = value;
-            this.paramRegexp = paramRegexp;
-        }
-
-        public Integer getPosition() {
-            return position;
-        }
-
-        public Object getValue() {
-            return value;
-        }
-
-        public String getParamRegexp() {
-            return paramRegexp;
-        }
-
-        public boolean isSingleValue() {
-            return !(value instanceof Collection || value instanceof Object[]);
-        }
-
-        public List<?> getMultipleValues() {
-            if (isSingleValue()) {
-                return Collections.singletonList(value);
-            } else {
-                if (value instanceof Collection) {
-                    return new ArrayList<Object>((Collection<?>) value);
-                } else if (value instanceof Object[]) {
-                    return Arrays.asList((Object[]) value);
-                }
-            }
-
-            return null;
-        }
     }
 }
