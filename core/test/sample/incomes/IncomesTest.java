42c76ca26291eb8e64a9611793b565bf511dee1d
degtyarjov <degtyarjov@haulmont.com>
2013-10-21 16:16:27 +0400

    tests

--- core/test/sample/incomes/IncomesTest.java
+++ core/test/sample/incomes/IncomesTest.java
@@ -9,11 +9,12 @@ import com.haulmont.yarg.reporting.RunParams;
 import com.haulmont.yarg.structure.Report;
 import com.haulmont.yarg.structure.xml.impl.DefaultXmlReader;
 import com.haulmont.yarg.util.groovy.DefaultScriptingImpl;
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileOutputStream;
+
+import static org.apache.commons.io.FileUtils.readFileToString;
 
 /**
  * @author degtyarjov
@@ -23,13 +24,16 @@ import java.io.FileOutputStream;
 public class IncomesTest {
     @Test
     public void testIncomesReport() throws Exception {
-        Report report = new DefaultXmlReader().parseXml(FileUtils.readFileToString(new File("./test/sample/incomes/incomes.xml")));
-        System.out.println();
+        Report report = new DefaultXmlReader()
+                .parseXml(readFileToString(new File("./test/sample/incomes/incomes.xml")));
 
         Reporting reporting = new Reporting();
         reporting.setFormatterFactory(new DefaultFormatterFactory());
-        reporting.setLoaderFactory(new DefaultLoaderFactory().setGroovyDataLoader(new GroovyDataLoader(new DefaultScriptingImpl())));
+        reporting.setLoaderFactory(
+                new DefaultLoaderFactory()
+                        .setGroovyDataLoader(new GroovyDataLoader(new DefaultScriptingImpl())));
 
-        ReportOutputDocument reportOutputDocument = reporting.runReport(new RunParams(report), new FileOutputStream("./result/sample/incomes.xlsx"));
+        ReportOutputDocument reportOutputDocument = reporting.runReport(
+                new RunParams(report), new FileOutputStream("./result/sample/incomes.xlsx"));
     }
 }
--- core/test/sample/invoice/InvoiceTest.java
+++ core/test/sample/invoice/InvoiceTest.java
@@ -7,6 +7,11 @@ import com.haulmont.yarg.reporting.ReportOutputDocument;
 import com.haulmont.yarg.reporting.Reporting;
 import com.haulmont.yarg.reporting.RunParams;
 import com.haulmont.yarg.structure.Report;
+import com.haulmont.yarg.structure.ReportBand;
+import com.haulmont.yarg.structure.ReportOutputType;
+import com.haulmont.yarg.structure.impl.BandBuilder;
+import com.haulmont.yarg.structure.impl.ReportBuilder;
+import com.haulmont.yarg.structure.impl.ReportTemplateBuilder;
 import com.haulmont.yarg.structure.xml.impl.DefaultXmlReader;
 import com.haulmont.yarg.util.groovy.DefaultScriptingImpl;
 import org.apache.commons.io.FileUtils;
@@ -32,4 +37,49 @@ public class InvoiceTest {
 
         ReportOutputDocument reportOutputDocument = reporting.runReport(new RunParams(report), new FileOutputStream("./result/sample/invoice.docx"));
     }
+
+
+    @Test
+    public void testInvoiceReportRaw() throws Exception {
+        ReportBuilder reportBuilder = new ReportBuilder();
+        ReportTemplateBuilder reportTemplateBuilder = new ReportTemplateBuilder()
+                .documentPath("./test/sample/invoice/invoice.docx")
+                .documentName("invoice.docx")
+                .outputType(ReportOutputType.docx)
+                .readFileFromPath();
+        reportBuilder.template(reportTemplateBuilder.build());
+        BandBuilder bandBuilder = new BandBuilder();
+        ReportBand main= bandBuilder.name("Main").query("Main", "return [\n" +
+                "                              [\n" +
+                "                               'invoiceNumber':99987,\n" +
+                "                               'client' : 'Google Inc.',\n" +
+                "                               'date' : new Date(),\n" +
+                "                               'addLine1': '1600 Amphitheatre Pkwy',\n" +
+                "                               'addLine2': 'Mountain View, USA',\n" +
+                "                               'addLine3':'CA 94043'\n" +
+                "                            ]]", "groovy").build();
+
+
+        bandBuilder = new BandBuilder();
+        ReportBand items = bandBuilder.name("Items").query("Items", "return [\n" +
+                "                                ['name':'Solar plasma', 'price' : 15000],\n" +
+                "                                ['name':'Flying tables', 'price' : 13000],\n" +
+                "                                ['name':'Black T-shirts', 'price' : 12000]\n" +
+                "                            ]", "groovy").build();
+
+        reportBuilder.band(main);
+        reportBuilder.band(items);
+
+        Report report = reportBuilder.build();
+
+        Reporting reporting = new Reporting();
+        reporting.setFormatterFactory(new DefaultFormatterFactory());
+        reporting.setLoaderFactory(
+                new DefaultLoaderFactory().setGroovyDataLoader(new GroovyDataLoader(new DefaultScriptingImpl())));
+
+        ReportOutputDocument reportOutputDocument = reporting.runReport(
+                new RunParams(report), new FileOutputStream("./result/sample/invoice.docx"));
+    }
+
+
 }
