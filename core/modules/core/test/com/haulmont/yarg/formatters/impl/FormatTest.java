e3f83cb0042ad9c8bc56f8f0e5c70a94158dd9e4
Yuriy Artamonov <artamonov@haulmont.com>
2018-05-22 14:55:27 +0400

    Fix NPE in FormatTest #71

--- core/modules/core/test/com/haulmont/yarg/formatters/impl/FormatTest.java
+++ core/modules/core/test/com/haulmont/yarg/formatters/impl/FormatTest.java
@@ -19,52 +19,59 @@ package com.haulmont.yarg.formatters.impl;
 import com.haulmont.yarg.formatters.factory.FormatterFactoryInput;
 import com.haulmont.yarg.structure.BandData;
 import com.haulmont.yarg.structure.ReportOutputType;
+import com.haulmont.yarg.structure.ReportTemplate;
 import com.haulmont.yarg.structure.impl.ReportFieldFormatImpl;
-import org.junit.Assert;
+import mockit.Mocked;
 import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
 
 public class FormatTest {
+    @Mocked
+    public ReportTemplate reportTemplate;
+
     @Test
     public void testNull() {
         AbstractFormatter abstractFormatter = createFormatter("a.number", "##.##");
-        Assert.assertEquals("", abstractFormatter.formatValue(null, "number", "a.number"));
+        assertEquals("", abstractFormatter.formatValue(null, "number", "a.number"));
     }
 
     @Test
     public void testNumber() {
         AbstractFormatter abstractFormatter = createFormatter("a.number", "##.##");
-        Assert.assertEquals("5,57", abstractFormatter.formatValue(5.5678, "number", "a.number").replace(".", ","));
+
+        assertEquals("5,57", abstractFormatter.formatValue(5.5678, "number", "a.number").replace(".", ","));
     }
 
     @Test
     public void testDate() throws ParseException {
         AbstractFormatter abstractFormatter = createFormatter("a.number", "yyyy-mm-dd");
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
-        Assert.assertEquals("2009-09-01", abstractFormatter.formatValue(simpleDateFormat.parse("01-09-2009"), "number", "a.number"));
+
+        assertEquals("2009-09-01", abstractFormatter.formatValue(simpleDateFormat.parse("01-09-2009"), "number", "a.number"));
     }
 
     @Test
     public void testClass() throws ParseException {
         AbstractFormatter abstractFormatter = createFormatter("a.number", "class:com.haulmont.yarg.formatters.impl.TestValueFormat");
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
-        Assert.assertEquals("Test", abstractFormatter.formatValue(null, "number", "a.number"));
-        Assert.assertEquals("Test", abstractFormatter.formatValue(12345, "number", "a.number"));
-        Assert.assertEquals("Test", abstractFormatter.formatValue(simpleDateFormat.parse("01-09-2009"), "number", "a.number"));
+
+        assertEquals("Test", abstractFormatter.formatValue(null, "number", "a.number"));
+        assertEquals("Test", abstractFormatter.formatValue(12345, "number", "a.number"));
+        assertEquals("Test", abstractFormatter.formatValue(simpleDateFormat.parse("01-09-2009"), "number", "a.number"));
     }
 
-
     private AbstractFormatter createFormatter(String formatName, String formatValue) {
-        final BandData rootBand = new BandData(BandData.ROOT_BAND_NAME);
-        rootBand.addReportFieldFormats(Arrays.asList(new ReportFieldFormatImpl(formatName, formatValue)));
-        final FormatterFactoryInput formatterFactoryInput = new FormatterFactoryInput("xlsx", rootBand, null, ReportOutputType.xlsx, null);
+        BandData rootBand = new BandData(BandData.ROOT_BAND_NAME);
+        rootBand.addReportFieldFormats(Collections.singletonList(new ReportFieldFormatImpl(formatName, formatValue)));
+        FormatterFactoryInput formatterFactoryInput = new FormatterFactoryInput("xlsx", rootBand, reportTemplate, ReportOutputType.xlsx, null);
         return new AbstractFormatter(formatterFactoryInput) {
             @Override
             public void renderDocument() {
-
             }
         };
     }
